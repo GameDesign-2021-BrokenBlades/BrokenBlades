894e7578a960ec42103520c8693db952ed8f7519
JMilamber <stringerj@allegheny.edu>
2021-04-19 04:42:49 -0400

    Reworked tile layout slightly, added player object, added player & ground collision, added player horizontal moevement and jump

---
+++ BrokenBlades/Assets/Scripts/player_movement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float moveSpeed = 7f;
+    public float jumpHeight = 12.5f;
+    public GameObject camera;
+    public bool isGrounded = false;
+    public bool isWalledLeft = false;
+    public bool isWalledRight = false;
+    void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+            Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+            if (isWalledLeft == false && isWalledRight == false){
+                transform.position += movement * Time.deltaTime * moveSpeed;
+            }
+            if (isWalledLeft == true & movement.x > 0){
+                transform.position += movement * Time.deltaTime * moveSpeed;
+            }
+            if (isWalledRight == true & movement.x < 0) {
+                transform.position += movement * Time.deltaTime * moveSpeed;
+            }
+
+            camera.transform.position = (transform.position + new Vector3(0f, 2.5f, -10));
+
+            if (isGrounded == true) {
+                Jump();
+            }
+
+    }
+
+    void Jump(){
+    if (Input.GetButtonDown("Jump")){
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpHeight), ForceMode2D.Impulse);
+        }
+    }
+}
